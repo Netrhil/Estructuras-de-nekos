be449667acfc4fcbae859d98bef9186bdc6b4d89
Saragar <saragar.jim@gmail.com>
2015-04-15 08:13:49 +0200

    Update Funciones.cpp

--- Funciones.cpp
+++ Funciones.cpp
@@ -24,30 +24,34 @@ void LlenarPol(Polinomio Pol[], int Num){
       cout<<" *----- Llenando las raizes -----*"<<endl;
 
     for(int z=0;z<Pol[i].Grado-2;z++){
-       cout<<"Ingrese las raizes del polinomio "<<z+1<<endl;
+       cout<<"Raiz Numero "<<z+1<<endl;
        cin>>Pol[i].Raizes[z];
     }
    }
+   cout<<endl;
  }
 
 void Mostrarpolinomios(Polinomio Pol[],int Num){
 
-cout<<" *----- Polinomios -----*"<<endl;
+cout<<" *-----  Mostrando Polinomios -----*"<<endl;
  for(int i=0;i<Num;i++){
     cout<<" *----- Polinomio numero "<<i+1<<" -----*"<<endl;
+      cout<<endl;
     cout<< " El grado del polinomio es "<<Pol[i].Grado<<endl;
 
     cout<<" *-----  Coeficientes -----*"<<endl;
+      cout<<endl;
     for(int j=0;j<Pol[i].Grado+1;j++){
        cout<<"Coeficientes de x elevado a "<<Pol[i].Grado-j<<" es " << Pol[i].Coeficientes[j]<<endl;
 
     }
       cout<<" *----- Raizes -----*"<<endl;
-     cout<<"Ingrese las raizes del polinomio son "<<endl;
+     cout<<" Las raizes del polinomio son "<<endl;
     for(int z=0;z<Pol[i].Grado-2;z++){
-       cout<<Pol[i].Raizes[z]<<endl;
+       cout<<Pol[i].Raizes[z]<<" , ";
     }
    }
+     cout<<endl;
  }
 
  void CalcularRaizes(float Arreglo[]){
@@ -55,5 +59,20 @@ cout<<" *----- Polinomios -----*"<<endl;
     result1=(-b+(sqrt(pow(b,2)-4*a*c) ))/(2*a);
     result2=(-b-(sqrt(pow(b,2)-4*a*c) ))/(2*a);
      cout<<result1<<" , "<<result2;
+
+ }
+
+ void ReducRufini(Polinomio Pol[],int Num){
+   float Raiz;
+    for(int i=0;i<Num;i++){
+        for(int j=0;j<Pol[i].Grado-2;j++){
+            Raiz=Pol[i].Raizes[j];
+            for(int z=0;z<Pol[i].Grado+1;z++){
+                Pol[i].Coeficientes[z+1]=Raiz*Pol[i].Coeficientes[z]+Pol[i].Coeficientes[z+1];
+           }
+
+        }
+       CalcularRaizes( Pol[i].Coeficientes);
+    }
 
  }
