ab1f1e6ea280c85271ef7807ff424fe775ed3bb8
Saragar <saragar.jim@gmail.com>
2015-04-15 06:24:10 +0200

    Create Funciones.h

---
+++ Funciones.h
@@ -0,0 +1,14 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+typedef struct{
+   int Grado;
+   float Coeficientes[];
+   float Raizes[];
+
+}Polinomio;
+void LlenarPol(Polinomio [], int );
+void Mostrarpolinomios(Polinomio [], int);
+void CalcularRaizes(float []);
+
+
+#endif // FUNCIONES_H_INCLUDED
